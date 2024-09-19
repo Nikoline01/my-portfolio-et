a222dddd02f1667ed7294023e539eee78ed923ec
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 07:45:12 +0200

    send

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,21 @@
+import NavBar from "../components/NavBar";
+import HeroSection from "../components/HeroSection";
+import Cards from "../components/Cards";
+import ToolBox from "../components/ToolBox";
+import Footer from "../components/Footer";
+import About from "../components/About";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroSection />
+      <Cards />
+      <ToolBox />
+      <About />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/ProjectPage.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "../components/NavBar";
+import ProjectCards from "../components/ProjectCards";
+import Footer from "../components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <HeroSection />
+      <ProjectCards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
