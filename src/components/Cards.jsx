b85c990bbbbbead01875d559bcdbb16c8bfaf38b
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 16:21:39 +0200

    Update Cards.jsx

---
+++ src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import CardsItem from "./CardsItem";
+
+export default function Cards() {
+  return (
+    <div className="cards">
+      <h1>PROJECTS</h1>
+      <ul className="cards-items">
+        <CardsItem />
+      </ul>
+    </div>
+  );
+}
